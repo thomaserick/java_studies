9811f4247f99cb8b5d5ab87dee6ed5f9691ac199
Thomas <thominhaserick@gmail.com>
2019-11-14 00:40:05 -0300

    Thread Runnable

--- java-basico/hello-world/src/cursojava/thread/AulaThread.java
+++ java-basico/hello-world/src/cursojava/thread/AulaThread.java
@@ -6,52 +6,48 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-			public void run() { /* Executa */
-				/* Código da Rotina */
-
-				/* Executar rotinas com uma faixa de tempo */
-
-				for (int pos = 0; pos < 10; pos++) {
-
-					System.out.println("Executanto rotina E-mail");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-
-			};
-
-		}.start(); /* Liga a Thread que ficara processando */
-
-		// ******** 2 Thread ********
-
-		new Thread() {
-
-			public void run() { /* Executa */
-
-				for (int pos = 0; pos < 10; pos++) {
-
-					System.out.println("Executanto rotina Validação");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-
-			};
-
-		}.start(); /* Liga a Thread que ficara processando */
+		Thread threadNfe = new Thread(thread2);
+		threadNfe.start();
 
 		System.out.println("Chegou o fim da Thread ");
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o Usuário");
 
 	}
 
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+
+				System.out.println("Executanto rotina Validação");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+
+				System.out.println("Executanto rotina E-mail");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	};
 }
