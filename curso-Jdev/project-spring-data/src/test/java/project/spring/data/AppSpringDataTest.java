9f0794275702c0fbe3480ae4f1ea6e676e918cd7
Thomas <thominhaserick@gmail.com>
2020-01-27 14:52:38 -0300

    config spring-jpa

---
+++ curso-Jdev/project-spring-data/src/test/java/project/spring/data/AppSpringDataTest.java
@@ -0,0 +1,19 @@
+package project.spring.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
+
+public class AppSpringDataTest {
+
+	@Test
+	public void testeInsert() {
+		System.out.println("Iniciou spring com Sucesso!");
+
+	}
+
+}
