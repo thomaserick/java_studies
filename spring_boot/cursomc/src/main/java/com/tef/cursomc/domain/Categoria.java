68e2a066780d54e7f4631f215822e0502e175b09
Thomas <thominhaserick@gmail.com>
2020-02-03 14:24:37 -0300

    Class domain Categoria

---
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Categoria.java
@@ -0,0 +1,73 @@
+package com.tef.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	
+	/**
+	 * Serializable -> os objetos são convertidos em sequencia de byts
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	
+	
+	public Categoria() {
+		
+	}
+	
+	
+	public Categoria(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	
+	//HashCode --> Comparados por conteúdo nao por ponteiro em memória
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+	
+}
--- spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-	
+
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {		
