cca4723c71dc7a9c08edf3038d215e7d537e1cd2
Thomas <thominhaserick@gmail.com>
2020-02-03 15:25:36 -0300

    Config Jpa class

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Categoria.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Categoria.java
@@ -2,6 +2,13 @@ package com.tef.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
@@ -9,7 +16,11 @@ public class Categoria implements Serializable{
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	
 	
