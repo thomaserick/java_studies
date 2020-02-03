669ef454f6457775b8cf8b4df3c874c8c98a8e10
Thomas <thominhaserick@gmail.com>
2020-02-03 16:53:00 -0300

    Create repository and Class products

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.tef.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 import com.tef.cursomc.domain.Categoria;
+import com.tef.cursomc.domain.Produto;
 import com.tef.cursomc.repositories.CategoriaRepository;
+import com.tef.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -17,17 +20,35 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+		
+		//	Criando o banco de dados Automaticamente
+		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",150.00);
+				
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 		
 		
 	}
--- spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Produto.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Produto.java
@@ -8,39 +8,43 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import org.hibernate.annotations.ManyToAny;
+
+
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 	
-	/**
-	 * Serializable -> os objetos são convertidos em sequencia de byts
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
 	private String name;
+	private Double preco;
+		
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+	joinColumns = @JoinColumn(name="produto_id"),
+	inverseJoinColumns = @JoinColumn(name="categoria_id")	
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	public Produto() {
 		
 	}
-	
-	
-	public Categoria(Integer id, String name) {
+
+	public Produto(Integer id, String name, Double preco) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.preco = preco;
 	}
-
 	
-	//HashCode --> Comparados por conteúdo nao por ponteiro em memória
+	
 
 	@Override
 	public int hashCode() {
@@ -50,7 +54,6 @@ public class Categoria implements Serializable{
 		return result;
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,7 +62,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -68,31 +71,37 @@ public class Categoria implements Serializable{
 		return true;
 	}
 
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
 
-	
-	
-	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+		
+
 }
