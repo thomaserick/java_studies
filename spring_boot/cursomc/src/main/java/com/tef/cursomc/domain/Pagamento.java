50339173c4b824cd846fff32f301eb8f6fccaa74
Thomas <thominhaserick@gmail.com>
2020-02-04 11:58:35 -0300

    Mapeamento @OneToOne

---
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Pagamento.java
@@ -0,0 +1,87 @@
+package com.tef.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import com.tef.cursomc.domain.enums.EstadoPagamento;
+
+
+@Entity
+public class Pagamento implements Serializable{
+		
+	private static final long serialVersionUID = 1L;
+	
+	@Id	
+	private Integer Id;
+	private EstadoPagamento estado;
+
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
+	private Pedido pedido;
+	
+	public Pagamento() {}
+
+	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+		super();
+		Id = id;
+		this.estado = estado;
+		this.pedido = pedido;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public EstadoPagamento getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPagamento estado) {
+		this.estado = estado;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Pedido.java
@@ -0,0 +1,111 @@
+package com.tef.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pedido implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private Date instanse;
+	
+	//Pecularidade do JPA
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")	
+	private Pagamento pagamento;
+	
+	private Cliente cliente;
+	
+	private Endereco enderecoEntrega;
+	
+	public Pedido() {}
+
+	public Pedido(Integer id, Date instanse, Pagamento pagamento, Cliente cliente, Endereco enderecoEntrega) {
+		super();
+		Id = id;
+		this.instanse = instanse;
+		this.pagamento = pagamento;
+		this.cliente = cliente;
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public Date getInstanse() {
+		return instanse;
+	}
+
+	public void setInstanse(Date instanse) {
+		this.instanse = instanse;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(Endereco enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
