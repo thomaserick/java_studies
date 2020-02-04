a478181ba5f3ea853fb8633491ebcac286bb5c16
Thomas <thominhaserick@gmail.com>
2020-02-04 16:27:15 -0300

    endpoint insert Category

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.tef.cursomc.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tef.cursomc.domain.Categoria;
 import com.tef.cursomc.services.CategoriaService;
@@ -21,11 +24,24 @@ public class CategoriaResource {
 	private CategoriaService categoriaservice;
 	
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {		
-	
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria categoria = categoriaservice.find(id);
-		return ResponseEntity.ok().body(categoria);	
 		
+		return ResponseEntity.ok().body(categoria);
 	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	//converte para o json para o objeto Java
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
+		categoria = categoriaservice.insert(categoria);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(categoria.getId()).toUri();		
+		return ResponseEntity.created(location).build();		
+	}
+	
+	
+	
+	
 
 }
--- spring_boot/cursomc/src/main/java/com/tef/cursomc/services/CategoriaService.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/services/CategoriaService.java
@@ -21,5 +21,10 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
+	
+	public Categoria insert(Categoria categoria) {
+		categoria.setId(null);
+		return categoriaRepository.save(categoria);
+	}
 
 }
