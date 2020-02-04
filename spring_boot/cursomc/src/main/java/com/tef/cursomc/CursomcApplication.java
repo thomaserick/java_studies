414c213077d713297a54aae0df11b0d449cad851
Thomas <thominhaserick@gmail.com>
2020-02-03 22:36:35 -0300

    implements cliente,tipoClient enum,enderecos

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
@@ -11,10 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 import com.tef.cursomc.domain.Categoria;
 import com.tef.cursomc.domain.Cidade;
+import com.tef.cursomc.domain.Cliente;
+import com.tef.cursomc.domain.Endereco;
 import com.tef.cursomc.domain.Estado;
 import com.tef.cursomc.domain.Produto;
+import com.tef.cursomc.domain.enums.TipoCliente;
 import com.tef.cursomc.repositories.CategoriaRepository;
 import com.tef.cursomc.repositories.CidadeRepository;
+import com.tef.cursomc.repositories.ClienteRepository;
+import com.tef.cursomc.repositories.EnderecoRepository;
 import com.tef.cursomc.repositories.EstadoRepository;
 import com.tef.cursomc.repositories.ProdutoRepository;
 
@@ -32,6 +37,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -78,6 +89,17 @@ public class CursomcApplication implements CommandLineRunner {
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
 		
 		
+		Cliente cli1 = new Cliente(null,"Maria","maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("14981050203","14981498410"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 300", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Maro", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+	
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 		
 		
---
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/domain/Cliente.java
@@ -0,0 +1,139 @@
+package com.tef.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.tef.cursomc.domain.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private String name;
+	private String email;
+	private String cpfCnpj;
+	private Integer tipo;
+		
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<Endereco>();
+	
+	//Nao aceita repetição
+	@ElementCollection
+	@CollectionTable(name="TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+	
+	public Cliente() {}
+
+	public Cliente(Integer id, String name, String email, String cpfCnpj, TipoCliente tipo) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+		this.cpfCnpj = cpfCnpj;
+		this.tipo = tipo.getCodigo();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCodigo();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+	
+	
+	
+
+}
