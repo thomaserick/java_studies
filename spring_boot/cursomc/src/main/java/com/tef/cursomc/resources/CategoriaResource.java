0d83f4ba8d605ca98273c6e8e350fce14dd362b4
Thomas <thominhaserick@gmail.com>
2020-02-04 21:24:58 -0300

    Personalizando retorno da validação

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.tef.cursomc.resources;
 
-
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,72 +23,65 @@ import com.tef.cursomc.dto.CategoriaDTO;
 import com.tef.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService categoriaservice;
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria categoria = categoriaservice.find(id);
-		
+
 		return ResponseEntity.ok().body(categoria);
 	}
-	
-	
+
 	@PostMapping
-	//converte para o json para o objeto Java
-	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO){
+	// converte para o json para o objeto Java
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
 		Categoria categoria = categoriaservice.fromDTO(categoriaDTO);
 		categoria = categoriaservice.insert(categoria);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(categoria.getId()).toUri();		
-		return ResponseEntity.created(location).build();		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
-	
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id){
-	categoria.setId(id);
-	categoria = categoriaservice.update(categoria);
-	return ResponseEntity.noContent().build();		
-		
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
-		 categoriaservice.delete(id);
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDTO, @PathVariable Integer id) {
+		Categoria categoria = categoriaservice.fromDTO(categoriaDTO);
+		categoria.setId(id);
+		categoria = categoriaservice.update(categoria);
 		return ResponseEntity.noContent().build();
-		
-		
+
 	}
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoriaservice.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List<Categoria> list = categoriaservice.findAll();			
-		
-		//Converte a lista para ListDto
-		List<CategoriaDTO> listDto = list.stream()
-				.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-		
-		
+		List<Categoria> list = categoriaservice.findAll();
+
+		// Converte a lista para ListDto
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/page",method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value="page",defaultValue = "0")Integer page, 
-			@RequestParam(value="linesPage",defaultValue = "24")Integer linesPage,
-			@RequestParam(value="orderBy",defaultValue = "name")String orderBy,
-			@RequestParam(value="direction",defaultValue = "ASC")String direction) {		
-		
-		
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPage", defaultValue = "24") Integer linesPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
 		Page<Categoria> list = categoriaservice.findPage(page, linesPage, orderBy, direction);
-		//Converte a lista para ListDto
-		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));				
+		// Converte a lista para ListDto
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
-	
 
 }
