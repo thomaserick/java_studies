9fcc81e4d8953f4e3dee1cc91e8d923863f8b53c
Thomas <thominhaserick@gmail.com>
2020-04-05 13:15:56 -0300

    implements Authorization

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/config/SecurityConfig.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.tef.cursomc.security.JWTAuthenticationFilter;
+import com.tef.cursomc.security.JWTAuthorizationFilter;
 import com.tef.cursomc.security.JWTUtil;
 
 @Configuration
@@ -64,6 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
