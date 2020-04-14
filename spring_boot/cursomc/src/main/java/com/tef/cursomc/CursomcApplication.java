6b46079a0d5bf72dd1ce653cf1cc7857ea3222fb
DevTef <thominhaserick@gmail.com>
2020-04-13 21:22:32 -0300

    Test upload S3

--- spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
+++ spring_boot/cursomc/src/main/java/com/tef/cursomc/CursomcApplication.java
@@ -10,8 +10,8 @@ import com.tef.cursomc.services.AwsS3Service;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private AwsS3Service awsS3Service;
+	//@Autowired
+	//private AwsS3Service awsS3Service;
 	
 	
 	public static void main(String[] args) {
@@ -20,8 +20,8 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		awsS3Service.uploadFile("C:\\temp\\cactus.jpg");
+		//Teste no amanzonS3 upload de imagem
+		//awsS3Service.uploadFile("C:\\temp\\cactus.jpg");
 
 	}
 
